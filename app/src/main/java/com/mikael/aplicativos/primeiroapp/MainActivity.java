36ad2ea08250d1d631a40fbeec3d36c0c21c02a1
MikaelSkd <mikael-silva2011@hotmail.com>
2022-03-28 08:33:07 -0300

    first part

--- app/src/main/java/com/mikael/aplicativos/primeiroapp/MainActivity.java
+++ app/src/main/java/com/mikael/aplicativos/primeiroapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.mikael.aplicativos.primeiroapp;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends ListActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.acoes));
+        setListAdapter(adapter);
     }
 }
