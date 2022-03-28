40f9a536359997ededaeca9714cf278a4b8a1c6c
MikaelSkd <mikael-silva2011@hotmail.com>
2022-03-28 08:26:21 -0300

    clear develop

--- app/src/main/java/com/mikael/aplicativos/primeiroapp/MainActivity.java
+++ app/src/main/java/com/mikael/aplicativos/primeiroapp/MainActivity.java
@@ -1,62 +1,14 @@
 package com.mikael.aplicativos.primeiroapp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final int REQUISICAO_ESTADO = 1;
-    private static final String ESTADO_SELECIONADO = "estado";
-
-    private Button btnSelecionar;
-    private String estado;
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnSelecionar = findViewById(R.id.btn_selecionar);
-        /*btnSelecionar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), TelaEstadosActivity.class);
-                intent.putExtra(TelaEstadosActivity.EXTRA_ESTADO, estado);
-                startActivity(intent);
-            }
-        });
-*/
-        btnSelecionar.setOnClickListener(view -> {
-            Intent intent = new Intent(getBaseContext(), TelaEstadosActivity.class);
-            intent.putExtra(TelaEstadosActivity.EXTRA_ESTADO, estado);
-            startActivityForResult(intent, REQUISICAO_ESTADO);
-        });
-
-        if (savedInstanceState != null){
-            estado = savedInstanceState.getString(ESTADO_SELECIONADO);
-            btnSelecionar.setText(estado);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == REQUISICAO_ESTADO){
-            estado = data.getStringExtra(TelaEstadosActivity.EXTRA_RESULTADO);
-            if (estado != null){
-                btnSelecionar.setText(estado);
-            }
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(ESTADO_SELECIONADO, estado);
     }
 }
