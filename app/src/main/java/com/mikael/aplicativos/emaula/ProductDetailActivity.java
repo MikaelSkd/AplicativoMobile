94f8c0de4f6ab7f98a9b6e1811a0d36e7a03e142
MikaelSkd <mikael-silva2011@hotmail.com>
2022-04-04 09:43:42 -0300

    Inicio da criacao do projeto loja virtual atletica

---
+++ app/src/main/java/com/mikael/aplicativos/emaula/ProductDetailActivity.java
@@ -0,0 +1,14 @@
+package com.mikael.aplicativos.emaula;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_produto_detalhe);
+    }
+}
---
+++ app/src/main/java/com/mikael/aplicativos/emaula/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.mikael.aplicativos.emaula;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                startActivity(intent);
+//                finish();
+//            }
+//        },3000);
+        handler.postDelayed(() -> {
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
