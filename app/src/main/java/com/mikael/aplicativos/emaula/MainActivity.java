6de6fe46a9fa89301f652e900ab59512fb38984e
MikaelSkd <mikael-silva2011@hotmail.com>
2022-04-11 10:56:46 -0300

    Adicionando a interface do Menu

--- app/src/main/java/com/mikael/aplicativos/emaula/MainActivity.java
+++ app/src/main/java/com/mikael/aplicativos/emaula/MainActivity.java
@@ -4,22 +4,103 @@ package com.mikael.aplicativos.emaula;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout produtoItem;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private TextView txtTitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        txtTitulo = findViewById(R.id.toolbar_titulo);
+        txtTitulo.setText(getString(R.string.app_name));
+
+        drawerLayout = findViewById(R.id.nav_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.toogle_open, R.string.toogle_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            Intent intent = null;
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+
+                case R.id.nav_account:
+                    Toast.makeText(MainActivity.this, "`Minha Conta", Toast.LENGTH_SHORT).show();
+//                    intent = new Intent(MainActivity.this, MainActivity.class);
+//                    startActivity(intent);
+                    break;
+
+                case R.id.nav_categoria:
+                    Toast.makeText(MainActivity.this, "`Categorias", Toast.LENGTH_SHORT).show();
+//                    intent = new Intent(MainActivity.this, MainActivity.class);
+//                    startActivity(intent);
+                    break;
+
+                case R.id.nav_pedidos:
+                    Toast.makeText(MainActivity.this, "`Meus Pedidos", Toast.LENGTH_SHORT).show();
+//                    intent = new Intent(MainActivity.this, MainActivity.class);
+//                    startActivity(intent);
+                    break;
+
+                case R.id.nav_carrinho:
+                    Toast.makeText(MainActivity.this, "`Meu Carrinho", Toast.LENGTH_SHORT).show();
+//                    intent = new Intent(MainActivity.this, MainActivity.class);
+//                    startActivity(intent);
+                    break;
+
+                case R.id.nav_logout:
+                    Toast.makeText(MainActivity.this, "`Sair", Toast.LENGTH_SHORT).show();
+//                    intent = new Intent(MainActivity.this, MainActivity.class);
+//                    startActivity(intent);
+                    break;
+
+                default:
+                    Toast.makeText(MainActivity.this, "Erro, tente novamente!", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
         produtoItem = findViewById(R.id.ll_produto_item);
         produtoItem.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, ProductDetailActivity.class);
             startActivity(intent);
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else
+            super.onBackPressed();
     }
 }
--- app/src/main/java/com/mikael/aplicativos/emaula/ProductDetailActivity.java
+++ app/src/main/java/com/mikael/aplicativos/emaula/ProductDetailActivity.java
@@ -1,17 +1,30 @@
 package com.mikael.aplicativos.emaula;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProductDetailActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto_detalhe);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        textView = findViewById(R.id.toolbar_titulo);
+        textView.setText("Titulo do Produto");
     }
 
     @Override
